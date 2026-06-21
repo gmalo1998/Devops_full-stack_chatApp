6ae0ce0ede86449956ed11c6f002a00d5bc0af6a
Ganesh Malo <malo.ganesh98@gmail.com>
2026-06-21 23:08:26 +0530

    Devops chatapp project

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -77,11 +77,22 @@ export const login = async (req, res) => {
 
 export const logout = (req, res) => {
   try {
-    res.cookie("jwt", "", { maxAge: 0 });
-    res.status(200).json({ message: "Logged out successfully" });
+    res.clearCookie("jwt", {
+      httpOnly: true,
+      sameSite: "lax",
+      secure: false,
+      path: "/",
+    });
+
+    res.status(200).json({
+      message: "Logged out successfully",
+    });
   } catch (error) {
     console.log("Error in logout controller", error.message);
-    res.status(500).json({ message: "Internal Server Error" });
+
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
   }
 };
 
--- backend/src/index.js
+++ backend/src/index.js
@@ -3,8 +3,6 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
-import path from "path";
-
 import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
@@ -12,19 +10,20 @@ import messageRoutes from "./routes/message.route.js";
 import healthRoutes from "./routes/health.route.js";
 import { app, server } from "./lib/socket.js";
 
-// Load environment variables from .env file
 dotenv.config();
 
 const PORT = process.env.PORT;
-const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
+
 app.use(
   cors({
-    origin: process.env.NODE_ENV === "production" 
-      ? ["http://localhost:8080", "http://localhost"] 
-      : "http://localhost:5173",
+    origin:
+      process.env.NODE_ENV === "production"
+        ? ["http://chatapp.example.com"]
+        : ["http://localhost:5173"],
+
     credentials: true,
   })
 );
@@ -33,13 +32,9 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/health", healthRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
+app.get("/", (req, res) => {
+  res.send("Backend running");
+});
 
 server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
